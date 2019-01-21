24191abb2f67bb9228cf4acade509f2d0192b6b7
choongman <maxx@encar.com>
2019-01-21 10:45:00 +0900

    small update

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,10 +3,10 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
   'src/components/Hello.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-hello" */ 'src/components/Hello.mdx'),
-  'src/components/Buttons/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-buttons-button" */ 'src/components/Buttons/Button.mdx'),
   'src/components/Input/Input.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-input-input" */ 'src/components/Input/Input.mdx'),
+  'src/components/Buttons/Button.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-buttons-button" */ 'src/components/Buttons/Button.mdx'),
   'src/components/Preloader/Preloader.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-preloader-preloader" */ 'src/components/Preloader/Preloader.mdx'),
 }
